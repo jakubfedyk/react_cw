c7f9597e4525d4bdcfa554670b794986ce493699
Jakub <jakubfedyk@gmail.com>
2017-11-18 10:47:38 +0100

    menu with logo

--- react-component/src/components/Header.js
+++ react-component/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Menu from './Menu'
+import Menu from './Menu';
+import logo from '../logo.svg';
+
 
 const Header = () => {
     return (
         <div>
             <h1>Header</h1>
             <Menu/>
+            <img src={logo}/>
         </div>)
 };
 export default Header;
