6497dac2d8840805f132dc65bddaf934433a109d
Jakub <jakubfedyk@gmail.com>
2017-11-18 11:05:29 +0100

    change style with js

--- react-component/src/components/Header.js
+++ react-component/src/components/Header.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Menu from './Menu';
 import logo from '../logo.svg';
-import './logo.css';
+// import './logo.css';
 
+const logoStyle = {
+    width: '100px',
+    height: '20px'
+};
+const header = {
+    fontSize: '75px',
+    fontFamily: 'Helvetica'
+};
 
 const Header = () => {
     return (
         <div>
-            <h1 className='header'>Header</h1>
+            <h1 style={header}>Header</h1>
             <Menu/>
-            <img className='logo' src={logo}/>
+            <img style={logoStyle} src={logo}/>
         </div>)
 };
 export default Header;
