c214bf50eb939c9ccf10304112ae91427030e095
Jakub <jakubfedyk@gmail.com>
2017-11-19 09:17:19 +0100

    examples with bootstrap react vol2

--- react-component/src/components/Home.js
+++ react-component/src/components/Home.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {Button} from 'react-bootstrap';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Grid, Row, Col, Button} from 'react-bootstrap';
 
 const Home = () => {
     const handleClick = () => {
         alert('handle click');
     };
-    return  (
+    return (
         <div>
-            <Button bsStyle="warning"  bsSize="large" onClick={handleClick}>Button</Button>
+            <Button bsStyle="warning" bsSize="large" onClick={handleClick}>Button</Button>
             <Grid>
-            <Row className="show-grid">
-                <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-                <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-            </Row>
+                <Row>
+                    <Col xs={6} md={8}>Column 1</Col>
+                    <Col xs={6} md={4}>Column 2</Col>
+                </Row>
             </Grid>
-
         </div>
     );
 };
--- react-component/src/components/Menu.js
+++ react-component/src/components/Menu.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import {Nav, NavItem} from 'react-bootstrap';
 
 
 const Menu = () => {
-    const menuElements = [
-        'Home','Profile','Dashboard'].map((element,index)=> {
-        return <li key={index}>{element}</li>;
-    });
     return (
         <div>
-            <ul>
-                {menuElements}
-            </ul>
+            <Nav bsStyle="pills" activeKey={0}>
+                <NavItem eventKey={1}>Home</NavItem>
+                <NavItem eventKey={2}>Profile</NavItem>
+                <NavItem eventKey={3}>Dashboard</NavItem>
+            </Nav>
         </div>)
 };
 export default Menu;
