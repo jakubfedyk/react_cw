853c950b5258ad77f2d0138547c5ff6e0601bb54
Jakub <jakubfedyk@gmail.com>
2017-11-19 08:25:56 +0100

    component Home

--- react-component/src/components/App.js
+++ react-component/src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home';
 
 const App = () => {
     return (
         <div>
             <Header/>
             <h2>Hello world</h2>
-            <p>Lorem Ipsum</p>
+            <Home/>
             <Footer/>
         </div>
     );
---
+++ react-component/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return  (
+        <div>Hello  World
+            <span>Lorem Ipsum</span>
+        </div>
+    );
+};
+
+export default Home
