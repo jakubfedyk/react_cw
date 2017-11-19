b41481e6189c3ebe2acabd0f2ddc66d36961b434
Jakub <jakubfedyk@gmail.com>
2017-11-19 10:19:02 +0100

    router react

--- react-component/src/components/App.js
+++ react-component/src/components/App.js
@@ -2,15 +2,33 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
+import Profile from './Profile';
+import Dashboard from './Dashboard';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <h2>Hello world</h2>
-            <Home/>
-            <Footer/>
-        </div>
-    );
+        <Router>
+            <div>
+                <Header/>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard"> Dashboard</Link>
+                    </li>
+                </ul>
+                <Route exact path='/' component={Home}/>
+                <Route path='/profile' component={Profile}/>
+                <Route path='/dashboard' component={Dashboard}/>
+                <Footer/>
+            </div>
+        </Router>
+    )
+        ;
 };
 export default App;
