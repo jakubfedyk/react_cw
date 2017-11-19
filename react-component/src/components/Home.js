b53bbc420397353bae3567db211c875d003a5f38
Jakub <jakubfedyk@gmail.com>
2017-11-19 09:06:51 +0100

    react-bootstrap examples

--- react-component/src/components/Home.js
+++ react-component/src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 const Home = () => {
     const handleClick = () => {
@@ -6,7 +8,14 @@ const Home = () => {
     };
     return  (
         <div>
-            <button className="btn btn-primary" onClick={handleClick}>Button</button>
+            <Button bsStyle="warning"  bsSize="large" onClick={handleClick}>Button</Button>
+            <Grid>
+            <Row className="show-grid">
+                <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+                <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+            </Row>
+            </Grid>
+
         </div>
     );
 };
--- react-component/src/index.js
+++ react-component/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 
 ReactDom.render(<App/>,document.getElementById('root'));
