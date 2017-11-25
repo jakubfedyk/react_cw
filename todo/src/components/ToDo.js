1d20a58d6a35a64b18f36b631f6e25c9b4c41d11
Jakub <jakubfedyk@gmail.com>
2017-11-25 13:37:02 +0100

    list of To do

--- todo/src/components/ToDo.js
+++ todo/src/components/ToDo.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            tasks: [
+                'Nauczyć się do testu',
+                'Zjeść śniadanie',
+                'Umyć okna'
+            ]
+        }
+
+    }
     render() {
         return <div>
             <h2>Things to do</h2>
+            <ol>
+                {this.state.tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ol>
         </div>
     }
 }
